bceac1cec17538cc5b72f793c299550bf48b1666
daman1209arora <45387992+daman1209arora@users.noreply.github.com>
2020-01-23 15:03:50 +0530

    Update RegisterFile.vhd

--- RegisterFile.vhd
+++ RegisterFile.vhd
@@ -4,11 +4,11 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity RegisterFile is
   PORT (
-    clk : IN STD_LOGIC;
+    clka : IN STD_LOGIC;
     wea : IN STD_LOGIC;
-    increment: IN STD_LOGIC;
     addra : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
     dina : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+    clkb : IN STD_LOGIC;
     enb : IN STD_LOGIC;
     addrb : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
     doutb : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
@@ -17,37 +17,31 @@ entity RegisterFile is
 end RegisterFile;
 
 architecture Behavioral of RegisterFile is
-	-- Reserve address 31 for the program counter.
-	-- Every time a '1' on increment is observed 
-	-- on the rising edge of the clock, increment  
-	-- memory(31) by 1. Reset sets it back to 0.
-	--		Confirm syntax in lab. 
 
-	type Memory_type is array (0 to 31) of std_logic_vector (31 downto 0);
+	type Memory_type is array (0 to 255) of std_logic_vector (31 downto 0);
 	signal Memory_array : Memory_type;
-	signal address : unsigned (7 downto 0);
-
+	signal address : unsigned (15 downto 0);
 begin
-	doutb <= Memory_array (to_integer(address));
-	process (clk)
+	process (clkb)
 	begin
-	    if rising_edge(clk) then    
-	        if (enb = '1') then
-	            address <= unsigned(addrb);    
-	        end if;	
-
-	        if(increment = '1')
-	        	Memory_array(31) <= Memory_array(31) + 1;
-	        end if;
-
-			if(reset = '1') then 
-				Memory_array(31) <= "00000000";
-			end if;
-
+    if rising_edge(clkb) then    
+        if (enb = '1') then
+            address <= unsigned(addrb);    
+        end if;
+    end if;
+    end process;
+	doutb <= Memory_array (to_integer(address));
+	
+	
+	process (clka)
+	begin
+		if rising_edge(clka) then	
+			if(reset = '1') then Memory_array(0) <= (others => '0');
+			else
 			if (wea = '1') then
-					Memory_array (to_integer(unsigned(addra))) <= dina (7 downto 0);	
+				Memory_array (to_integer(unsigned(addra))) <= dina;	
+			end if;
 		    end if;
-
 		end if;
 	end process;
 end Behavioral;
