10e9e76c85588f4890e617c0fa7f603dbd83d0b2
daman1209arora <45387992+daman1209arora@users.noreply.github.com>
2020-01-23 16:50:15 +0530

    Update RegisterFile.vhd

--- RegisterFile.vhd
+++ RegisterFile.vhd
@@ -1,51 +1,46 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.NUMERIC_STD.ALL;
+use IEEE.numeric_std.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity RegisterFile is
   PORT (
     clk : IN STD_LOGIC;
     wea : IN STD_LOGIC;
-    increment: IN STD_LOGIC;
-    addra : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+    addra : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
     dina : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+    
     enb : IN STD_LOGIC;
-    addrb : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
-    doutb : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
-    reset: IN std_logic
+    addrb1 : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+    doutb1 : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
+    
+    addrb2 : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+    doutb2 : OUT STD_LOGIC_VECTOR(31 DOWNTO 0)
   );
 end RegisterFile;
 
 architecture Behavioral of RegisterFile is
-	-- Reserve address 31 for the program counter.
-	-- Every time a '1' on increment is observed 
-	-- on the rising edge of the clock, increment  
-	-- memory(31) by 1. Reset sets it back to 0.
-	--		Confirm syntax in lab. 
 
 	type Memory_type is array (0 to 31) of std_logic_vector (31 downto 0);
 	signal Memory_array : Memory_type;
-	signal address : unsigned (7 downto 0);
+	signal address1 : unsigned (7 downto 0);
+    signal address2 : unsigned (7 downto 0);
 
 begin
-	doutb <= Memory_array (to_integer(address));
+	doutb2 <= Memory_array (to_integer(address2));
+	doutb1 <= Memory_array (to_integer(address1));
+	
+	
 	process (clk)
 	begin
 	    if rising_edge(clk) then    
 	        if (enb = '1') then
-	            address <= unsigned(addrb);    
+	            address2 <= unsigned(addrb2);
+	            address1 <= unsigned(addrb1);    
 	        end if;	
 
-	        if(increment = '1')
-	        	Memory_array(31) <= Memory_array(31) + 1;
-	        end if;
-
-			if(reset = '1') then 
-				Memory_array(31) <= "00000000";
-			end if;
-
 			if (wea = '1') then
-					Memory_array (to_integer(unsigned(addra))) <= dina (7 downto 0);	
+                Memory_array (to_integer(unsigned(addra))) <= dina;	
 		    end if;
 
 		end if;
