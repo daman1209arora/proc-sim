41c9e2a9bc9ef0d8fc505f0ecfb0a1a7ce71766f
daman1209arora <45387992+daman1209arora@users.noreply.github.com>
2020-01-24 01:34:57 +0530

    Update ControlUnit.vhd

--- ControlUnit.vhd
+++ ControlUnit.vhd
@@ -1,5 +1,7 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.numeric_std.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity ControlUnit is
   Port (clk: IN STD_LOGIC;
@@ -8,16 +10,15 @@ entity ControlUnit is
 end ControlUnit;
 
 architecture Behavioral of ControlUnit is
-    signal instr: STD_LOGIC_VECTOR(1 downto 0);
+    signal aluMode: STD_LOGIC_VECTOR(1 downto 0);
     signal rfenb: STD_LOGIC;
     signal rfwea: STD_LOGIC;
-    signal rfaddr1: STD_LOGIC_VECTOR(7 downto 0);
-    signal rfaddr2: STD_LOGIC_VECTOR(7 downto 0);
-    signal rfWrite: STD_LOGIC_VECTOR(7 downto 0);    
+    signal rfaddr1: STD_LOGIC_VECTOR(4 downto 0);
+    signal rfaddr2: STD_LOGIC_VECTOR(4 downto 0);
+    signal rfWrite: STD_LOGIC_VECTOR(4 downto 0);    
     signal reg1: STD_LOGIC_VECTOR(31 downto 0);
     signal reg2: STD_LOGIC_VECTOR(31 downto 0);
     signal regWrite: STD_LOGIC_VECTOR(31 downto 0);
-
     
     signal muwea: STD_LOGIC;
     signal muenb: STD_LOGIC;
@@ -25,10 +26,8 @@ architecture Behavioral of ControlUnit is
     signal muaddrw: STD_LOGIC_VECTOR(15 downto 0);
     signal muin: STD_LOGIC_VECTOR(31 downto 0);
     signal muout: STD_LOGIC_VECTOR(31 downto 0);
-    
-    
+ 
     signal aluenb: STD_LOGIC;
-    signal aluMode: STD_LOGIC_VECTOR(1 downto 0);
     
     
     component ALU is port(
@@ -39,27 +38,30 @@ architecture Behavioral of ControlUnit is
          );
     end component;
     
-    component MU is port(
+    component Memory is port(
             clka,clkb, wea, enb : IN STD_LOGIC;
             addra, addrb : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
-            dina, doutb : IN STD_LOGIC_VECTOR(31 DOWNTO 0)
+            dina : IN STD_LOGIC_VECTOR(31 downto 0);
+            doutb : OUT STD_LOGIC_VECTOR(31 DOWNTO 0)
         );
     end component;
     
-    component RF is port(
+    component RegisterFile is port(
         clk, wea, enb : IN STD_LOGIC;
-        addra, addrb1, addrb2 : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
-        dina, doutb1, doutb2 : IN STD_LOGIC_VECTOR(31 DOWNTO 0)
+        addra, addrb1, addrb2 : IN STD_LOGIC_VECTOR(4 DOWNTO 0);
+        dina : IN STD_LOGIC_VECTOR(31 downto 0);
+        doutb1, doutb2 : OUT STD_LOGIC_VECTOR(31 DOWNTO 0)
         );
     end component;
-    type p_state is (idle, loadingInstruction, decode, aluRegistersLoaded, swLoaded, lwLoaded, swLoadFinish, lwLoadFinish, increment, stopped);
-    constant zero: STD_LOGIC_VECTOR := (others => '0');
+    type p_state is (idle, loadingaluModeuction, decode, aluRegistersLoaded, aluDone, swLoaded, lwLoaded, registerOverwritten, swLoadFinish, lwLoadFinish, increment, stopped);
+    constant zero: STD_LOGIC_VECTOR(31 downto 0) := (others => '0');
     signal state: p_state := idle;
-    signal progCounter : integer := 0;
+    signal progCounter : integer := 1;
+    signal aluOut : std_logic_vector(31 downto 0);
 begin
-    alu_comp: ALU port map(clk => clk, enb => aluenb, R1 => reg1, R2 =>reg2, mode => instr, O1 => regWrite);
-    mu_comp: MU port map(clka => clk, clkb => clk, wea => muwea, enb => muenb, addra => muaddrr, addrb => muaddrw, dina => reg1, doutb => regWrite);
-    rf_comp: RF port map(clk =>clk, wea => rfwea, enb => rfenb, addra => rfWrite, addrb1 => rfaddr1, addrb2 => rfaddr2, dina => regWrite, doutb1 => reg1, doutb2 => reg2);
+    alu_comp: ALU port map(clk => clk, enb => aluenb, R1 => reg1, R2 =>reg2, mode => aluMode, O1 => aluOut);
+    mu_comp: Memory port map(clka => clk, clkb => clk, wea => muwea, enb => muenb, addra => muaddrw, addrb => muaddrr, dina => reg1, doutb => regWrite);
+    rf_comp: RegisterFile port map(clk =>clk, wea => rfwea, enb => rfenb, addra => rfWrite, addrb1 => rfaddr1, addrb2 => rfaddr2, dina => aluOut, doutb1 => reg1, doutb2 => reg2);
 
 
     process(clk)
@@ -69,19 +71,20 @@ begin
                 if(start = '1') then
                     muenb <= '1';
                     muaddrr <= std_logic_vector(to_unsigned(progCounter, 16));
+                    state <= loadingaluModeuction;
                 end if;
             -- Just after this clock cycle, the signal muenb will be enabled. 
-            -- The next program instruction will be loaded into regWrite after two clock cycles.
+            -- The next program aluModeuction will be loaded into regWrite after two clock cycles.
 
 
-            elsif(state = loadingInstruction) then 
-                state = decode;
-            -- Just after this clock cycle, the instruction has been loaded into regWrite
+            elsif(state = loadingaluModeuction) then 
+                state <= decode;
+            -- Just after this clock cycle, the aluModeuction has been loaded into regWrite
 
 
             elsif(state = decode) then
                 if(regWrite = zero) then
-                    state = stopped;
+                    state <= stopped;
                 elsif(regWrite(31 downto 26) = "000000") then 
                     aluenb <= '1';
                     if(regWrite(5 downto 0) = "000000") then
@@ -93,31 +96,37 @@ begin
                     elsif(regWrite(5 downto 0) = "100010") then 
                         aluMode <= "01";
                     else
-                        state <= stoppped;
+                        state <= stopped;
                     end if;
                     rfenb <= '1';
                     rfaddr1 <= regWrite(25 downto 21);
                     rfaddr2 <= regWrite(20 downto 16);
-                    muaddrw <= regWrite(15 downto 11);
                     state <= aluRegistersLoaded;
                 elsif(regWrite(31 downto 26) = "101011") then
                     rfenb <= '1';
                     rfaddr1 <= regWrite(25 downto 21);
                     state <= swLoaded;
-                    muaddrw <= regWrite(15 downto 11);
+                    muaddrw <= regWrite(15 downto 0);
                 elsif(regWrite(31 downto 26) = "100011") then
                     muenb <= '1';
-                    muaddrr <= regWrite(25 downto 21);
+                    muaddrr <= regWrite(15 downto 0);
                     state <= lwLoaded;
                     rfaddr2 <= regWrite(15 downto 11);
                 else
-                    state = stopped;
+                    state <= stopped;
                 end if;
 
             elsif(state = aluRegistersLoaded) then
                 aluenb <= '0';
                 state <= aluDone;
-
+    
+            elsif(state = aluDone) then
+                rfenb <= '1';
+                state <= registerOverwritten;
+            
+            elsif(state = registerOverwritten) then     
+                state <= increment;
+                
             elsif(state = swLoaded) then 
                 muwea <= '1';
                 state <= swLoadFinish;
@@ -138,7 +147,7 @@ begin
             
             elsif(state = increment) then
                 progCounter <= progCounter + 1;
-                state <= start;
+                state <= idle;
             end if;
 
         end if;
