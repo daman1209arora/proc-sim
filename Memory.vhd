991360ba23e971bdc4842e4bece7f71f539c8830
daman1209arora <45387992+daman1209arora@users.noreply.github.com>
2020-01-25 04:10:41 +0530

    Update Memory.vhd

--- Memory.vhd
+++ Memory.vhd
@@ -5,9 +5,14 @@ use IEEE.NUMERIC_STD.ALL;
 entity Memory is
   PORT (
     clka : IN STD_LOGIC;
-    wea : IN STD_LOGIC;
-    addra : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
-    dina : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+    wea1 : IN STD_LOGIC;
+    addra1 : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+    dina1 : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+    
+    wea2 : IN STD_LOGIC;
+    addra2 : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+    dina2 : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+    
     clkb : IN STD_LOGIC;
     enb : IN STD_LOGIC;
     addrb1: IN STD_LOGIC_VECTOR(15 DOWNTO 0);
@@ -21,11 +26,17 @@ end Memory;
 architecture Behavioral of Memory is
 
 	type Memory_type is array (0 to 4095) of std_logic_vector (31 downto 0);
-	signal Memory_array : Memory_type := (
-	                                       0 => "10001100000000010000010000000000",
-	                                       1000 => (2 => '1', others => '0'), 
-	                                       others => "00000000000000000000000000000000") ;
-	                                       --0 => "10101100000000000000010000000000",
+	signal Memory_array : Memory_type := (  0 => "00000000000000000000010101000000",
+                                            1 => "10001100000001000000000001101000",
+                                            2 => "00000000001000100000100000100000",
+                                            3 => "00000000011000110001100000100000",
+                                            4 => "10101100000000000000000001100011",
+                                            5 => "00000000000000110001100011000010",
+                                            100 => "00000000000000000000000001110000",
+                                            102 => "00000000000000000000000000101101",
+                                            103 => "00000000000000000000000000000110",
+                                            104 => "00000000000000001111111111111110",
+                                            others => (others => '0')) ;
 	signal address1, address2 : unsigned (15 downto 0);
 begin
 	process (clkb)
@@ -39,15 +50,15 @@ begin
     end process;
 	doutb1 <= Memory_array (to_integer(address1));
 	doutb2 <= Memory_array (to_integer(address2));
-	--doutb <= (others => '1');
 	
 	process (clka)
 	begin
 		if rising_edge(clka) then	
 			if(reset = '1') then Memory_array(0)<= (others => '0');
 			else
-			if (wea = '1') then
-				Memory_array (to_integer(unsigned(addra))) <= dina;	
+			if (wea1 = '1') then
+				Memory_array (to_integer(unsigned(addra1))) <= dina1;
+				Memory_array (to_integer(unsigned(addra2))) <= dina2;	
 			end if;
 		    end if;
 		end if;
